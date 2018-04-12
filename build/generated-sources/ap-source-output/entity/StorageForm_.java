c47cb1e2ee19a6e38841d4e20d95be218f074623
Samir Ahmed <samir82show@hotmail.com>
2018-04-12 15:57:17 +0300

    StorageOps - Thu, Apr 12, 2018  3:57:17 PM

--- build/generated-sources/ap-source-output/entity/StorageForm_.java
+++ build/generated-sources/ap-source-output/entity/StorageForm_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-12T15:09:06")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-12T15:53:15")
 @StaticMetamodel(StorageForm.class)
 public class StorageForm_ { 
 
